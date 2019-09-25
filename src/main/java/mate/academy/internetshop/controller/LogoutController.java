c0a039d1258eed3345808ecfc6b79b1f5c7bf803
ghkhrshch <george.khruschov@gmail.com>
2019-09-25 12:10:10 +0300

    Session invalidate fixed

--- src/main/java/mate/academy/internetshop/controller/LogoutController.java
+++ src/main/java/mate/academy/internetshop/controller/LogoutController.java
@@ -12,9 +12,8 @@ public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        HttpSession session = req.getSession(true);
-        session.removeAttribute("userId");
-        session.invalidate();
+        req.getSession().removeAttribute("userId");
+        req.getSession().invalidate();
         for (Cookie cookie : req.getCookies()) {
             if (cookie.getName().equals("MATE")) {
                 cookie.setMaxAge(0);
